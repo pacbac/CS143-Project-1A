461c4dfc74895b6b3a4d97bb1af7d822f192dbe4
George Zhang <georgezhang@ucla.edu>
2019-01-24 15:32:53 -0800

    add table

--- query.php
+++ query.php
@@ -1,30 +1,49 @@
 <!DOCTYPE html>
 <?php
-   $query = $_GET['query']; // get the query from the get request
-   // establish connection with db
-   $db_connection = mysql_connect("localhost", "cs143", "");
-   mysql_select_db("TEST", $db_connection); //
-   ?>
+$query = $_GET['query']; // get the query from the get request
+// establish connection with db
+$db_connection = mysql_connect("localhost", "cs143", "");
+mysql_select_db("TEST", $db_connection); //
+?>
 <html>
   <body>
     <h1>Enter your SQL query here:</h1>
-    // form data, pressing submit will rerun the page with the textarea query
+    <!-- form data, pressing submit will rerun the page with the textarea query -->
     <form action="query.php" method="GET">
       <textarea name="query" cols="60" rows="8"><?php print "$query" ?></textarea>
       <br />
       <input type="submit" value="Submit" />
-   </form>
+    </form>
     <br/>
-    // print out data
-    <?php 
-       $rs = mysql_query($query, $db_connection);
-       while($row = mysql_fetch_row($rs)){
-       for($i = 0; $i < sizeof($row); $i++){
-	echo "$row[$i] ";
-	}
-	echo "<br/>";
-       }
-       mysql_close($db_connection);
-       ?>
+    <!-- print out data -->
+    <?php
+    // sends query and rs stores array of results
+    $rs = mysql_query($query, $db_connection);
+    // if (!$rs) {
+    //   die('Query failed: ' . mysql_error());
+    // } else {
+    if ($rs) {
+      echo "<table border=1 cellspacing=1 cellpadding=2>";
+      $i = 0;
+      echo "<tr align=center>";
+      while ($i < mysql_num_fields($rs)) {
+        $meta = mysql_fetch_field($rs, $i);
+        echo "<th>$meta->name</th>";
+        $i++;
+      }
+      echo "</tr>";
+      while ($row = mysql_fetch_row($rs)) {
+        echo "<tr align=center>";
+        for ($i = 0; $i < sizeof($row); $i++) {
+          $nonnull = ($row[$i] ? $row[$i] : "N/A");
+          echo "<td>$nonnull</td>";
+        }
+        echo "</tr>";
+        mysql_close($db_connection);
+      }
+      echo "</table>";
+    }
+    // }
+    ?>
   </body>
 </html>
